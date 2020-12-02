130fc6384e8e94e7f7e8433c566032f49c77a486
Santa Rosa Distribuidora <yuriramoscc@gmail.com>
2020-12-02 10:48:24 -0300

    Brincando

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './estilos.css';
 import Carousel from 'react-bootstrap/Carousel';
+import './estilos.css';
 class Slider extends Component {
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <Carousel>
-                <Carousel.Item interval={2000}>
+            <Carousel className='carousel'>
+                <Carousel.Item>
                     <a href="https://www.santarosapaper.com.br/">
                         <img
                             className="d-block w-100"
@@ -19,7 +19,7 @@ class Slider extends Component {
                     <Carousel.Caption>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={2000}>
+                <Carousel.Item>
                     <a href="https://api.whatsapp.com/send?l=pt_br&phone=5541984894802">
                         <img
                             className="d-block w-100"
@@ -30,7 +30,7 @@ class Slider extends Component {
                     <Carousel.Caption>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={2000}>
+                <Carousel.Item>
                     <a href="https://www.linkedin.com/in/yuri-ramos-841212173/">
                         <img
                             className="d-block w-100"
--- src/components/TelaInicial/TelaInicial.jsx
+++ src/components/TelaInicial/TelaInicial.jsx
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 import Slider from '../Slider';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card';
+
 class TelaInicial extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
             <section>
-                <Slider/>
+                <Slider />
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Title>Locações</Card.Title>
+                                <Card.Subtitle>Propostas conforme a sua demanda</Card.Subtitle>
+                                <Card.Text>
+                                    Samsung 4070, Samsung 4080, Epson 5710, Canon 6030.
+                                </Card.Text>
+                                <Card.Link href="https://www.santarosapaper.com.br/locacoes">Clique aqui</Card.Link>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Title>Materiais para Escritório</Card.Title>
+                                <Card.Subtitle>Organização, Materiais e muito mais</Card.Subtitle>
+                                <Card.Text>
+                                    Calculadoras, Canetas, Estiletes, Pranchetas, Colas, Corretivos e muito mais.
+                                </Card.Text>
+                                <Card.Link href="https://www.santarosapaper.com.br/materiais-para-escritorio">Clique aqui</Card.Link>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Title>Papelaria</Card.Title>
+                                <Card.Subtitle>Ideal para o seu escritório.</Card.Subtitle>
+                                <Card.Text>
+                                    Papéis brancos em caixa, resmas de papéis coloridos, papéis fotográficos...
+                                </Card.Text>
+                                <Card.Link href="https://www.santarosapaper.com.br/papelaria">Clique aqui</Card.Link>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </section>
         );
     }
 }
- 
+
 export default TelaInicial;
