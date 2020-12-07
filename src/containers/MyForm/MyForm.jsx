4f79b45e125aaf11c268ef1fb098ebb8dda7262c
Santa Rosa Distribuidora <yuriramoscc@gmail.com>
2020-12-07 17:10:23 -0300

    meu deus como eu amo react

--- src/containers/MyForm/MyForm.jsx
+++ src/containers/MyForm/MyForm.jsx
@@ -14,37 +14,38 @@ class MyForm extends Component {
     render() {
         return (
             <>
-            <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+                <Button variant="primary" onClick={() => this.handleModalShowHide()}>
                     Editar
                 </Button>
-            <Modal show={this.state.showHide}>
-                <Modal.Header>{this.state.impressora.customer_name}</Modal.Header>
-                <Modal.Body>
-                    <form action="#" method="post">
-                        <div class="row">
-                            <div class="form-group col">
-                                <select class="form-control" id="scan_status" name="scan_status" placeholder="Selecione o Status">
-                                    <option selected>Nenhum</option>
-                                    <option>André</option>
-                                    <option>Impossível checagem presencial</option>
-                                    <option>Sem monitoramento</option>
-                                    <option>Novo (em observação)</option>
-                                    <option>Passado para Keven</option>
-                                    <option>Não precisa conferir</option>
-                                </select>
+                <Modal show={this.state.showHide}>
+                    <Modal.Header>{this.state.impressora.customer_name}</Modal.Header>
+                    <Modal.Body>
+                        <form action="#" method="post">
+                            <div class="row">
+                                <div class="form-group col">
+                                    <select value={this.state.impressora.scan_status} class="form-control" id="scan_status" name="scan_status" placeholder="Selecione o Status">
+                                        <option value=''>Nenhum</option>
+                                        <option value='andre'>André</option>cantCheck
+                                        <option value='devilPrinter'>Impossível!!</option>
+                                        <option value='cantCheck'>Impossível checagem presencial</option>
+                                        <option value='noMonitoring'>Sem monitoramento</option>
+                                        <option value='recentlyLost'>Novo (em observação)</option>
+                                        <option value='keven'>Passado para Keven</option>
+                                        <option value='everythingOk'>Não precisa conferir</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="descricao">Observações:</label>
-                            <textarea cols="20" rows="10" id="scan_observation" name="scan_observation" placeholder="Adicione observações..." class="form-control" value={this.state.impressora.scan_observation}></textarea>
-                        </div>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-            <input type='submit' value='Cancelar' class='btn btn-primary'onClick={() => this.handleModalShowHide()}/>
-                <input type="submit" value="Salvar" class="btn btn-primary" onClick={() => this.handleModalShowHide()}/>
-                </Modal.Footer>
-            </Modal>
+                            <div class="form-group">
+                                <label for="descricao">Observações:</label>
+                                <textarea cols="20" rows="10" id="scan_observation" name="scan_observation" placeholder="Adicione observações..." class="form-control" value={this.state.impressora.scan_observation}></textarea>
+                            </div>
+                        </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <input type='submit' value='Cancelar' class='btn btn-danger' onClick={() => this.handleModalShowHide()} />
+                        <input type="submit" value="Salvar" class="btn btn-success" onClick={() => this.handleModalShowHide()} />
+                    </Modal.Footer>
+                </Modal>
             </>
         );
     }
